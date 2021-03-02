9d32fc9c6e429d3afcd2e9556b2338f34e1d8130
igor vinicius Freitas de Souza <igorviniciusfreitasouza@gmail.com>
2021-03-02 07:18:19 -0300

    Update classe_aluno_metodo.cpp

--- classe_aluno_metodo.cpp
+++ classe_aluno_metodo.cpp
@@ -1,3 +1,13 @@
+/*
+
+Aluno Método é um pequeno problema proposto em uma aula que pega dados e imprime estes dados e a média do usuário.
+*/
+/*
+* Author:  Igor Vinicius Freitas de Souza
+* GitHub: https://github.com/igor1043
+* E-mail: igorviniciusfreitasouza@gmail.com
+*/
+
 #include<iostream>
 #include<string>
 using namespace std;
